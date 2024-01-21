d5d28742c64d9632f449e9ed06b96f55830dca98
Leonardo Vergara Márquez <leonardo11vergara11@gmail.com>
2024-01-21 18:34:14 -0500

    fix json import

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,11 +1,13 @@
+import json from "../json/questions.json" assert { type: "json" };
+
 const API_URL = "https://retoolapi.dev/5WH3sb/domande-aperte";
-const API_URL_LOCAL = "/assets/json/questions.json";
+// const API_URL_LOCAL = "/assets/json/questions.json";
 
 export async function getQuestions() {
+  return json;
   try {
-    return await fetch(API_URL_LOCAL).then((response) => response.json());
     return await fetch(API_URL).then((response) => response.json());
   } catch (err) {
-    return await fetch(API_URL_LOCAL).then((response) => response.json());
+    return json;
   }
 }
